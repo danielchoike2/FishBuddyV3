c1645008a0e0cd461a8b70bf0cbadd4196962bb4
danielchoike2 <choike3@mail.nmc.edu>
2023-10-30 21:54:17 -0400

    Add project files.

---
+++ Models/FishLog.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace FishBuddy.Models
+{ 
+ 
+    public class FishLog
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Date")]
+        public string? Date { get; set; }
+
+        [Required]
+        [Display(Name = "Fish Name")]
+        [StringLength(30, ErrorMessage = "Please enter a fish name 30 characters or less.")]
+        public string? FishName { get; set; }
+
+        [Display(Name = "Location")]
+        public string? Location { get; set; }
+
+        [Display(Name = "Length")]
+        public string? MaxLength { get; set; }
+
+        [Display(Name = "Weight")]
+        public string? MaxWeight { get; set; }
+
+       
+
+
+    }
+}
